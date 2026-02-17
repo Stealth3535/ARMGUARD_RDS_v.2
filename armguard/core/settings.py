da4dbdfd959eeb5b192cc7a3e2d7df88ea929ada
Stealth3535 <september192016143@gmail.com>
2026-02-18 02:49:31 +0800

    Stop duplicate static discovery from core/static

--- armguard/core/settings.py
+++ armguard/core/settings.py
@@ -424,7 +424,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
+STATICFILES_DIRS = []
 
 # Static files optimization
 STATICFILES_FINDERS = [
